62f2fbc19b64499cb90f32775fdd057f6c1ac5a1
john.rodriguez <john.rodriguez@gmail.com>
2023-08-02 21:58:17 -0500

    Adding dto object to user model

---
+++ user-management/user.core/src/main/java/com/springbank/user/core/models/Account.java
@@ -0,0 +1,18 @@
+package com.springbank.user.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+    private String userName;
+    private String passWord;
+    private List<Role> role;
+}
