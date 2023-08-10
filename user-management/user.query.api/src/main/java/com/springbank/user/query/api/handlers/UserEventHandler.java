31920206005f8569297f24a6c6dbc1db390a5813
john.rodriguez <john.rodriguez@gmail.com>
2023-08-09 21:41:38 -0500

    Creating events

---
+++ user-management/user.query.api/src/main/java/com/springbank/user/query/api/handlers/UserEventHandler.java
@@ -0,0 +1,12 @@
+package com.springbank.user.query.api.handlers;
+
+import com.springbank.user.core.events.UserRegisteredEvent;
+import com.springbank.user.core.events.UserRemovedEvent;
+import com.springbank.user.core.events.UserUpdateEvent;
+
+public interface UserEventHandler {
+
+    void on(UserRegisteredEvent event);
+    void on(UserUpdateEvent event);
+    void on(UserRemovedEvent event);
+}
---
+++ user-management/user.query.api/src/main/java/com/springbank/user/query/api/handlers/UserEventHandlerImpl.java
@@ -0,0 +1,41 @@
+package com.springbank.user.query.api.handlers;
+
+import com.springbank.user.core.events.UserRegisteredEvent;
+import com.springbank.user.core.events.UserRemovedEvent;
+import com.springbank.user.core.events.UserUpdateEvent;
+import com.springbank.user.query.api.repositories.UserRepository;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@ProcessingGroup("${processing.group}")
+public class UserEventHandlerImpl implements UserEventHandler{
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserEventHandlerImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @EventHandler
+    @Override
+    public void on(UserRegisteredEvent event) {
+        userRepository.save(event.getUser());
+
+    }
+
+    @EventHandler
+    @Override
+    public void on(UserUpdateEvent event) {
+        userRepository.save(event.getUser());
+    }
+
+    @EventHandler
+    @Override
+    public void on(UserRemovedEvent event) {
+        userRepository.deleteById(event.getId());
+    }
+}
